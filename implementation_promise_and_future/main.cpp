0cb558ac8bd8026fa7f07c43c51c6a87bc82159d
i.kardanov <i.kardanov@wisetech.pro>
2022-09-02 12:17:54 +0300

    добавил новый тест на многопоточное исполнение задач через future

--- implementation_promise_and_future/main.cpp
+++ implementation_promise_and_future/main.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <sstream>
 #include <thread>
+#include <cassert>
 
 #include "package_task.h"
 
@@ -19,7 +20,7 @@ void PrintMessage(const std::string &msg) {
 using namespace async;
 
 
-void firstExample() {
+void FirstExample() {
     auto promise = Promise<int>();
     auto future = promise.getFuture();
 
@@ -38,23 +39,71 @@ void firstExample() {
     PrintMessage(oss.str());
 }
 
-void secondExample() {
+void SecondExample() {
     auto task = [](){
-        SleepSecond(10);
+//        SleepSecond(20);
         const auto someValue = 1000;
         PrintMessage("Promise is ready!");
         return someValue;
     };
+    Promise<int> promise;
+    std::thread otherThread([promise = std::move(promise), task = std::move(task)]() mutable {
+        promise.set(task());
+    });
 
-    PackageTask<int()> packageTask(task);
+
     PrintMessage("Call future get method");
-    const auto result = packageTask.getFuture().get();
+    const auto result = promise.getFuture().get();
     std::stringstream oss;
     oss << "Result from future with value: " << result;
     PrintMessage(oss.str());
 }
 
+void ThirdExample() {
+    const auto value = 1000;
+    auto task = [value](){
+        SleepSecond(1);
+        PrintMessage("Future value is ready!");
+        return value;
+    };
+
+    auto packageTask = PackageTask<int()>(task);
+    auto future = packageTask.getFuture();
+
+    SleepSecond(10);
+    PrintMessage("Call future get method");
+    const auto result = future.get();
+    std::stringstream oss;
+    oss << "Result from future with value: " << result;
+    PrintMessage(oss.str());
+
+    assert(result == value);
+}
+
+void SomeTestOnMultithreading() {
+    auto globalCounter = 0;
+    const auto capacity = 10000;
+    std::vector<Promise<int>::Future> futures = {};
+    futures.reserve(capacity);
+
+    for (auto i = 0; i < capacity; ++i) {
+        auto task = [&globalCounter](){
+            return ++globalCounter;
+        };
+        auto packageTask = PackageTask<int()>(task);
+        futures.push_back(packageTask.getFuture());
+    }
+
+    std::for_each(futures.begin(), futures.end(), [](auto &future){
+        const auto result = future.get();
+        std::stringstream oss;
+        oss << "Result from future with value: " << result;
+        PrintMessage(oss.str());
+    });
+    assert(globalCounter != capacity);
+}
+
 int main() {
-    secondExample();
+    SomeTestOnMultithreading();
     return 0;
 }
